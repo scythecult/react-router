51f6104e45e0b3e10a8300c7b101dfff2d908a28
scythecult <scythecult@gmail.com>
2022-08-22 19:33:03 +0300

    initial styling components commit

--- src/components/card/card.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./card.css";
-
-const Card = (props) => {
-  return <li className="card">{props.children}</li>;
-};
-
-export { Card };
--- src/components/placeholder/placeholder.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./placeholder.css";
-
-const Placeholder = () => {
-  return <p className="expense-list-placeholder">No expenses yet :)</p>;
-};
-
-export { Placeholder };
