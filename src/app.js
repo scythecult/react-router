f6f5c93554f7cff1ae492ba56869ef2e505f421d
scythecult <scythecult@gmail.com>
2022-08-28 17:56:05 +0300

    -init second practice react project

--- src/app.js
+++ src/app.js
@@ -1,34 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./app.module.css";
-import { Modal } from "./components/modal/modal";
-import { UserForm } from "./components/user-form/user-form";
-import { UserList } from "./components/user-list/user-list";
-import { useStoredUsers } from "./hooks";
 
 const App = () => {
-  const [users, setUsers] = useStoredUsers();
-  const [isError, setIsError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const updateUsers = (user) => {
-    setUsers((prevUsers) => [...prevUsers, user]);
-  };
-
-  const modal = isError ? (
-    <Modal toggleModal={setIsError} message={errorMessage} />
-  ) : null;
-
-  return (
-    <div className={styles.app}>
-      <UserForm
-        updateUsers={updateUsers}
-        toggleModal={setIsError}
-        updateErrorMessage={setErrorMessage}
-      />
-      <UserList items={users} />
-      {modal}
-    </div>
-  );
+  return <div className={styles.app}></div>;
 };
 
-export default App;
+export { App };
