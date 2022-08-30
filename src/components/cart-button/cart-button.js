f08533c5521c0257a5da5a477c87e25e7a361f06
scythecult <scythecult@gmail.com>
2022-08-30 13:14:44 +0300

    -added animation to add-to-cart-button component

--- src/components/cart-button/cart-button.js
+++ src/components/cart-button/cart-button.js
@@ -1,13 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { CartContext } from "../../cart-context/cart-context";
 import styles from "./cart-button.module.css";
 import { CartIcon } from "./cart-icon";
 
 const CartButton = () => {
   const { cartCount } = useContext(CartContext);
+  const [animationClass, setAnimationClass] = useState("");
+
+  useEffect(() => {
+    setAnimationClass(styles.bump);
+
+    const timerId = setTimeout(() => {
+      setAnimationClass("");
+    }, 300);
+
+    return () => clearTimeout(timerId);
+  }, [cartCount]);
 
   return (
-    <button className={styles.button}>
+    <button className={`${styles.button} ${animationClass}`}>
       <CartIcon className={styles.icon} />
       Your Cart
       <span className={styles.badge}>{cartCount}</span>
