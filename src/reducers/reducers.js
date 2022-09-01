a680995e10875859c54f0ce07edcd9fe4b47bc95
scythecult <scythecult@gmail.com>
2022-09-01 15:01:23 +0300

    -improved cart-reducer logic for interacting with the cart

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,29 +1,63 @@
 const cartReducer = (state, action) => {
   switch (action.type) {
     case "ADD_MEAL_TO_CART": {
-      const targetMealIndex = state.meals.findIndex(
-        (meal) => meal.id === action.payload.id
+      const mealIndex = state.meals.findIndex((meal) => meal.id === action.payload.id);
+      const targetMeal = state.meals[mealIndex];
+
+      const newMeal = {
+        ...targetMeal,
+        quantity: action.payload.mealCount,
+      };
+
+      const cartItemIndex = state.cartItems.findIndex(
+        (item) => item.id === targetMeal.id
       );
-      const targetMeal = state.meals[targetMealIndex];
-      const newMeal = { ...targetMeal, quantity: action.payload.mealCount };
-      const cartItems = [...state.cartItems, newMeal];
+
+      let cartItems = [];
+
+      if (cartItemIndex !== -1) {
+        const cartItem = state.cartItems[cartItemIndex];
+        const updatedCartItem = {
+          ...cartItem,
+          quantity: cartItem.quantity + action.payload.mealCount,
+        };
+
+        cartItems = [...state.cartItems];
+
+        return {
+          ...state,
+          cartItems: [
+            ...cartItems.slice(0, cartItemIndex),
+            updatedCartItem,
+            ...cartItems.slice(cartItemIndex + 1),
+          ],
+        };
+      }
+
+      cartItems = [newMeal, ...state.cartItems];
 
       return { ...state, cartItems };
     }
 
     case "REMOVE_MEAL_FROM_CART": {
-      const targetMealIndex = state.cartItems.findIndex(
-        (item) => item.id === action.payload
-      );
-      const targetMeal = state.cartItems[targetMealIndex];
+      const mealIndex = state.cartItems.findIndex((item) => item.id === action.payload);
+      const targetMeal = state.cartItems[mealIndex];
       const newMeals = [...state.cartItems];
-      newMeals.splice(targetMealIndex, 1);
 
       if (targetMeal.quantity > 1) {
         const newMeal = { ...targetMeal, quantity: targetMeal.quantity - 1 };
 
-        return { ...state, cartItems: [...newMeals, newMeal] };
+        return {
+          ...state,
+          cartItems: [
+            ...newMeals.slice(0, mealIndex),
+            newMeal,
+            ...newMeals.slice(mealIndex + 1),
+          ],
+        };
       }
+
+      newMeals.splice(mealIndex, 1);
 
       return { ...state, cartItems: [...newMeals] };
     }
