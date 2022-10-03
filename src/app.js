4d412dec76482b0ca6d1b89257e5519f6db7bfb0
scythecult <scythecult@gmail.com>
2022-10-03 16:56:28 +0300

    -added rendering logic

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { Auth } from "./components/Auth";
 import { Counter } from "./components/Counter";
 import { Header } from "./components/Header";
+import { UserProfile } from "./components/UserProfile";
 import { Users } from "./components/Users";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
     <>
       <Header />
       {!isLoggedIn && <Auth />}
+      {isLoggedIn && <UserProfile />}
       <Counter />
       <Users />
     </>
